9f0ac8308d32bc745e34b960a1247a73ea3b6b1b
lebang <443664360@qq.com>
2017-03-21 00:21:11 +0800

    sqlite_3
    practice database

--- uiwindow/ModuleViewController.m
+++ uiwindow/ModuleViewController.m
@@ -7,11 +7,20 @@
 //
 
 #import "ModuleViewController.h"
+#import "sqlite3.h"
 
 @interface ModuleViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *moduleName;
 @property (weak, nonatomic) IBOutlet UITextField *edtPracticed;
+
+@property (copy,nonatomic) NSString *databaseFilePath;
+#define kDatabaseName @"database.sqlite3"
+
 - (IBAction)doCommit:(id)sender;
+- (IBAction)insert:(id)sender;
+- (IBAction)update:(id)sender;
+- (IBAction)delete:(id)sender;
+- (IBAction)query:(id)sender;
 
 @end
 
@@ -20,17 +29,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.moduleName.text = self.name;
-}
-
--(void)viewWillDisappear:(BOOL)animated{
-    NSLog(@"done something 1");
-    self.name = self.edtPracticed.text;
-    if ([self.mDelegate respondsToSelector:@selector(setString:)]) {
-        [self.mDelegate setString:self.name];
-        NSLog(@"done something 2");
+    
+    //获取数据库路径
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    self.databaseFilePath = [documentsDirectory stringByAppendingPathComponent:kDatabaseName];
+    //打开或创建数据库
+    sqlite3 *database;
+    if (sqlite3_open([self.databaseFilePath UTF8String], &database) != SQLITE_OK) {
+        sqlite3_close(database);
+        NSLog(@"打开数据库失败");
     }
-    [self dismissViewControllerAnimated:YES completion:nil];
-    NSLog(@"done something 3");
+    //创建数据表
+    NSString *sql_create = @"CREATE TABLE IF NOT EXISTS TABLE_LEBANG(NAME TEXT, SEX TEXT);";
+    char *errorMsg;
+    if (sqlite3_exec(database, [sql_create UTF8String], NULL, NULL, &errorMsg) != SQLITE_OK) {
+        sqlite3_close(database);
+        NSLog(@"数据库建表失败");
+    }
+    
+    //插入
+    sqlite3_stmt *stmt;
+    char *sql_insert = "INSERT INTO TABLE_LEBANG(NAME,SEX)VALUES(?,?);";
+    if (sqlite3_prepare_v2(database, sql_insert, -1, &stmt, nil) == SQLITE_OK) {
+        //        sqlite3_bind_int(stmt, 1, 1);
+        sqlite3_bind_text(stmt, 1, [self.moduleName.text UTF8String], -1, NULL);
+        sqlite3_bind_text(stmt, 2, [self.moduleName.text UTF8String], -1, NULL);
+    }else{
+        sqlite3_close(database);
+        NSLog(@"数据库准备插入失败");
+    }
+    
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        sqlite3_finalize(stmt);
+        NSLog(@"数据库插入失败");
+    }
+    
+    //查询
+    NSString *sql_query = @"SELECT NAME,SEX FROM TABLE_LEBANG";
+    sqlite3_stmt *statement;
+    if (sqlite3_prepare_v2(database, [sql_query UTF8String], -1, &statement, nil) == SQLITE_OK) {
+        while (sqlite3_step(statement) == SQLITE_ROW) {
+            char *name = (char *)sqlite3_column_text(statement, 0);
+            char *sex = (char *)sqlite3_column_text(statement, 1);
+            NSLog(@"数据库查出 %s ||| %s",name,sex);
+        }
+        sqlite3_finalize(statement);
+    }
+    
+    sqlite3_close(database);
 }
 
 /*
@@ -52,5 +99,17 @@
     }
     [self dismissViewControllerAnimated:YES completion:nil];
     NSLog(@"done something 3");
+}
+
+- (IBAction)insert:(id)sender {
+}
+
+- (IBAction)update:(id)sender {
+}
+
+- (IBAction)delete:(id)sender {
+}
+
+- (IBAction)query:(id)sender {
 }
 @end
